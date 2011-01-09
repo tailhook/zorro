7c7f8ebbb0dc86dd5bc7d8691128f7eb1d0409aa
Paul Colomiets <pc@gafol.net>
2011-01-09 12:50:25 +0200

    Added `setup.py`

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='Zorro',
+      version='0.1',
+      description='Network communication library, with zeromq support',
+      author='Paul Colomiets',
+      author_email='pc@gafol.net',
+      url='http://github.com/tailhook/zorro',
+      classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        ],
+      packages=['zorro'],
+    )
