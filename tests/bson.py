e5a24eb699d1c1b641bfee60cff7628ecb49c239
Paul Colomiets <paul@colomiets.name>
2011-11-22 00:17:32 +0200

    mongodb: piece of bson serialization implemented

--- tests/bson.py
+++ tests/bson.py
@@ -4,12 +4,23 @@ from zorro.mongodb import bson
 class TestBson(unittest.TestCase):
 
     def test_loads(self):
-        data1 = b"\x16\x00\x00\x00\x02hello\x00\x06\x00\x00\x00world\x00\x00"
-        self.assertEqual({"hello": "world"}, bson.loads(data1))
-        data2 = (b"1\x00\x00\x00\x04BSON\x00&\x00\x00\x00\x020\x00\x08\x00\x00"
+        data = b"\x16\x00\x00\x00\x02hello\x00\x06\x00\x00\x00world\x00\x00"
+        self.assertEqual({"hello": "world"}, bson.loads(data))
+        data = (b"1\x00\x00\x00\x04BSON\x00&\x00\x00\x00\x020\x00\x08\x00\x00"
                  b"\x00awesome\x00\x011\x00333333\x14@\x102\x00\xc2\x07\x00"
                  b"\x00\x00\x00")
-        self.assertEqual({"BSON": ["awesome", 5.05, 1986]}, bson.loads(data2))
+        self.assertEqual({"BSON": ["awesome", 5.05, 1986]}, bson.loads(data))
+
+    def test_dumps(self):
+        data = {"hello": "world"}
+        coded = b"\x16\x00\x00\x00\x02hello\x00\x06\x00\x00\x00world\x00\x00"
+        self.assertEqual(coded, bson.dumps(data))
+        coded = (b"1\x00\x00\x00\x04BSON\x00&\x00\x00\x00\x020\x00\x08\x00\x00"
+                 b"\x00awesome\x00\x011\x00333333\x14@\x102\x00\xc2\x07\x00"
+                 b"\x00\x00\x00")
+        data = {"BSON": ["awesome", 5.05, 1986]}
+        self.assertEqual(coded, bson.dumps(data))
+
 
 if __name__ == '__main__':
     unittest.main()
--- zorro/mongodb/bson.py
+++ zorro/mongodb/bson.py
@@ -2,11 +2,19 @@ import struct
 from itertools import count
 
 _unpack_dict = {}
+_pack_dict = {}
 
 
 def _unpack(char):
     def register(fun):
         _unpack_dict[char] = fun
+        return fun
+    return register
+
+
+def _pack(char, typ):
+    def register(fun):
+        _pack_dict[typ] = (char, fun)
         return fun
     return register
 
@@ -20,8 +28,9 @@ def unpack_float(buf, idx):
 def unpack_string(buf, idx):
     ln, = struct.unpack_from('<i', buf, idx)
     idx += 4
-    end = buf.index(b'\x00', idx)
-    return buf[idx:end].decode('utf-8'), end+1
+    if buf[idx+ln-1] != 0:
+        raise ValueError("String serialization is broken")
+    return buf[idx:idx+ln-1].decode('utf-8'), idx+ln
 
 
 @_unpack(0x03)
@@ -83,3 +92,66 @@ def loads(s):
             raise ValueError("Garbage at end of document")
     return obj
 
+
+@_pack(0x01, float)
+def pack_float(value, buf):
+    buf.extend(struct.pack('<d', value))
+
+
+@_pack(0x02, str)
+def pack_string(value, buf):
+    val = value.encode('utf-8')
+    buf.extend(struct.pack('<i', len(val)+1))
+    buf.extend(val)
+    buf.append(0)
+
+
+@_pack(0x03, dict)
+def pack_document(value, buf):
+    pos = len(buf)
+    buf.extend('\x00\x00\x00\x00')
+    _pack_doc(value.items(), buf)
+    buf.append(0)
+    struct.pack_into('<i', buf, pos, len(buf) - pos)
+
+
+@_pack(0x04, list)
+def pack_array(value, buf):
+    pos = len(buf)
+    buf.extend(b'\x00\x00\x00\x00')
+    _pack_doc(((str(i), v)
+               for i, v in enumerate(value)), buf)
+    buf.append(0)
+    struct.pack_into('<i', buf, pos, len(buf) - pos)
+
+
+@_pack(0x10, int)
+def pack_int(value, buf):
+    if value > (1 << 31) or value < -(1 << 31):
+        raise NotImplementedError("Long integers not implemented")
+    buf.extend(struct.pack('<i', value))
+
+
+def _pack_doc(pairs, buf):
+    for k, v in pairs:
+        spec = _pack_dict.get(type(v))
+        if spec is None:
+            for typ in _pack_dict:
+                if isinstance(v, typ):
+                    spec = _pack_dict[typ]
+                    break
+            else:
+                raise ValueError("Can't BSONize "+type(v))
+        ch, fun = spec
+        buf.append(ch)
+        buf.extend(k.encode('utf-8'))
+        buf.append(0)
+        fun(v, buf)
+
+
+def dumps(obj):
+    buf = bytearray(4)
+    _pack_doc(obj.items(), buf)
+    buf.append(0)
+    struct.pack_into('<i', buf, 0, len(buf))
+    return bytes(buf)
