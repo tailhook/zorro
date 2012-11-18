148f82caa6593bafff8343413abaaa5324b29f15
Paul Colomiets <paul@colomiets.name>
2012-11-18 18:35:01 +0200

    Add `Redirect` exceptions

--- zorro/web.py
+++ zorro/web.py
@@ -71,6 +71,9 @@ class Request(object):
 class WebException(Exception):
     """Base for all exceptions which render error code (and page) to client"""
 
+    @abc.abstractmethod
+    def default_response(self):
+        pass
 
 class Forbidden(WebException):
 
@@ -138,6 +141,56 @@ class MethodNotAllowed(WebException):
                     b'</body>'
                 b'</html>'
                 )
+
+
+class Redirect(WebException):
+
+    def __init__(self, location, status_code, status_text):
+        self.statusline = '{:d} {}'.format(status_code, status_text)
+        self.location = location
+
+    def location_header(self):
+        return b'Location\0' + self.location.encode('ascii') + b'\0'
+
+    def headers(self):
+        return (b'Content-Type\0text/html\0'
+                + self.location_header())
+
+    def default_response(self):
+        return (self.statusline, self.headers(),
+                '<!DOCTYPE html>'
+                '<html>'
+                    '<head>'
+                        '<title>{0.statusline}</title>'
+                    '</head>'
+                    '<body>'
+                    '<h1>{0.statusline}</h1>'
+                    '<a href="{0.location}">Follow</a>'
+                    '</body>'
+                '</html>'.format(self)
+                )
+
+
+class CompletionRedirect(Redirect):
+    """Temporary redirect which sends code 303
+
+    With :param:`cookie` set it is often used for login forms. Without
+    parameter set it is used to provide "success" page for various web forms
+    and other non-idempotent actions
+    """
+
+    def __init__(self, location, cookie=None, *,
+        status_code=303, status_text="See Other"):
+        super().__init__(location,
+            status_code=status_code, status_text=status_text)
+        self.cookie = cookie
+
+    def headers(self):
+        sup = super().headers()
+        if self.cookie is not None:
+            sup += ('Set-Cookie\0'
+                    + self.cookie.output(header='').encode('ascii') + '\0')
+        return sup
 
 
 class ChildNotFound(Exception):
